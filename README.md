28152f2cf9c10ddeb6b5a9b470afb61fa77a25ce
Anthony Ye <anthonyye3662@hotmail.com>
2021-06-24 12:33:53 -0400

    performed local stress testing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sdc
+
+# engineering journal
+
+# Local Stress testing
+- Stress testing done on styles route with randomized product ids
+- 1 in 2000 requests fail at 100 requests per second
+- 1 in 250 requests fail at 200 requests per second
+- Reliability is worst on the first 500 requests after server is started, 1 in 25 fail at 200 rps, goes down to 1 in 200 on the second batch of 500 requests.
+- Failure point seems to be Promise.all, then block running before promise results array fully populates
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require('axios');
 const bodyParser = require("body-parser");
 const product = require('./database/product.js');
 const style = require('./database/style.js');
@@ -15,14 +16,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 
-
+//2 to 7 ms
 app.get('/products', (req, res) => {
+  var startTime = Date.now();
   var page = parseInt(req.query.page) || 1;
   var count = parseInt(req.query.count) || 5;
   var minIndex = (page - 1) * count + 1;
   var maxIndex = minIndex + count;
   product.findAll(minIndex, maxIndex)
     .then((data) => {
+      var endTime = Date.now();
+      console.log('/products time:', endTime - startTime);
       res.json(data);
     })
     .catch((err) => {
@@ -31,7 +35,9 @@ app.get('/products', (req, res) => {
     });
 });
 
+//2 to 7 ms
 app.get('/products/:product_id', (req, res) => {
+  var startTime = Date.now();
   var pid = req.params.product_id;
   var promises = [];
   promises.push(product.find(pid));
@@ -41,17 +47,18 @@ app.get('/products/:product_id', (req, res) => {
     var item = JSON.parse(JSON.stringify(results[0]));
     var featuresArr = results[1];
     item['features'] = featuresArr;
+    var endTime = Date.now();
+    console.log('product detail time:', endTime - startTime);
     res.json(item);
   })
   .catch((err) => {
     console.log(err);
     res.sendStatus(500);
   });
-
-
 });
-
+//10 to 24 ms
 app.get('/products/:product_id/styles', (req, res) => {
+  var startTime = Date.now();
   var pid = req.params.product_id;
   style.find(pid)
   .then((response) => {
@@ -74,11 +81,17 @@ app.get('/products/:product_id/styles', (req, res) => {
     })
     .then(() => {
       Promise.all(skuPromises)
+      //first point of failure under high stress - seems the then block is running before the
+      //promise results array fully populates
       .then((skus) => {
         // console.log(skus);
         for (var i = 0; i < styles.length; i++) {
           var skusObject = {};
           var skuArray = skus[i];
+          if (!skuArray) {
+            console.log(pid);
+            console.log(skus);
+          }
           for (var j = 0; j < skuArray.length; j++) {
             var key = skuArray[j].sku;
             var innerObject = {quantity: skuArray[j].quantity, size: skuArray[j].size};
@@ -90,7 +103,13 @@ app.get('/products/:product_id/styles', (req, res) => {
           product_id: pid,
           results: styles
         };
+        var endTime = Date.now();
+        // console.log(pid + ' styles time:', endTime - startTime);
         res.json(output);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
       });
     });
   })
@@ -100,19 +119,46 @@ app.get('/products/:product_id/styles', (req, res) => {
   });
 });
 
+//3 to 7 ms
 app.get('/products/:product_id/related', (req, res) => {
+  var startTime = Date.now();
   var pid = req.params.product_id;
   relationship.find(pid)
   .then((relations) => {
     var output = relations.map((relation) => {
       return relation.related_product_id;
     });
+    var endTime = Date.now();
+    console.log('related time:', endTime - startTime);
     res.json(output);
   })
   .catch((err) => {
     console.log(err);
     res.sendStatus(500);
   });
+});
+
+app.get('/test', (req, res) => {
+  var results = [0,0]; //success,error
+  var repeater = setInterval(() => {
+    var random = Math.floor(Math.random() * 1000000) + 1;
+    axios.get(`http://127.0.0.1:3000/products/${random}/styles`)
+    .then((response) => {
+      // console.log(response.data)
+      results[0] += 1;
+    })
+    .catch((err) => {
+      // console.log(err);
+      results[1] += 1;
+    });
+  }, 5);
+  setTimeout(() => {
+    clearInterval(repeater);
+    setTimeout(() => {
+      res.json(results);
+    }, 50);
+
+  }, 2500);
 });
 
 
